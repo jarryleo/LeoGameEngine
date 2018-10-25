1fedff80ded62fffd24a0ac25cc3451bfeba14d5
liujiarui <liujiarui@ship56.net>
2018-10-25 15:39:10 +0800

    优化示例代码

--- app/src/main/java/cn/leo/leogameengine/game/FirstScene.java
+++ app/src/main/java/cn/leo/leogameengine/game/FirstScene.java
@@ -68,20 +68,16 @@ public class FirstScene extends BaseScene {
                 .setAnimClip(animClip, true)
                 //设置元素大小
                 .setWidth(60)
-                .setHeight(80);
-        //设置元素位置
-        animCell.setX((getWidth() / 2) - (animCell.getWidth() / 2))
-                .setY(getHeight() - animCell.getHeight() * 2);
+                .setHeight(80)
+                //设置元素位置
+                .setCenterToX(getWidth() / 2)
+                .setCenterToY(getHeight() - 120);
         //元素添加到帧
         layer.addCell(animCell);
-
-        //元素添加到控制器
+        //元素添加到控制器,便于其它位置获取飞机元素
         getCellControl().addCell("player", animCell);
-
-        //飞机触摸控制
-        final CellControl.CellProperty player = getCellControl().getCellProperty("player").get(0);
-
-        getTouchControl().setCellOnTouch(player.getCell(), new CellOnTouchListener() {
+        //飞机触摸事件监听
+        getTouchControl().setCellOnTouch(animCell, new CellOnTouchListener() {
             @Override
             public boolean onTouch(BaseCell cell, CellMotionEvent event) {
                 int action = event.getAction();
@@ -94,8 +90,7 @@ public class FirstScene extends BaseScene {
                         break;
                     case MotionEvent.ACTION_MOVE:
                     case MotionEvent.ACTION_UP:
-                        BaseCell playerCell = player.getCell();
-                        playerCell.moveTo(x - mDx, y - mDy);
+                        cell.moveTo(x - mDx, y - mDy);
                         break;
                     default:
                         break;
@@ -110,23 +105,29 @@ public class FirstScene extends BaseScene {
      * 子弹
      */
     private void createBullet(BaseLayer layer) {
+        //创建子弹对象
         final ImageCell bullet = ImageCell.build(this, "pic/bullet1.png")
                 .setWidth(5, true)
                 .setY(250)
                 .setVisible(false);
+        //交给控制器
         layer.addCell(bullet);
         getCellControl().addCell("bullet", bullet);
+        //克隆10发交给控制器
         for (int i = 0; i < 10; i++) {
             ImageCell clone = bullet.clone();
             clone.setY(250 + i * 100);
             layer.addCell(clone);
             getCellControl().addCell("bullet", clone);
         }
+        //子弹速度
         getCellControl().setYSpeed("bullet", -300);
+        //获取玩家位置,给子弹初始左边
         final CellControl.CellProperty player = getCellControl().getCellProperty("player").get(0);
-        getCellControl().setCellEventListener("bullet", new CellEventListener() {
+        //子弹事件监控
+        getCellControl().setCellEventListener("bullet", new CellEventListener<ImageCell>() {
             @Override
-            public void onCellMove(BaseCell cell, float lastX, float newX, float lastY, float newY, float lastRotation, float newRotation) {
+            public void onCellMove(ImageCell cell, float lastX, float newX, float lastY, float newY, float lastRotation, float newRotation) {
                 if (newY < -cell.getHeight()) {
                     float x = player.getCell().getX();
                     float y = player.getCell().getY();
@@ -154,6 +155,7 @@ public class FirstScene extends BaseScene {
                 .setWidth(120)
                 .setHeight(180)
                 .setCenterToX(getWidth() / 2);
+        //克隆敌机
        /* Random random = new Random();
         for (int i = 0; i < 100; i++) {
             AnimCell clone = animCell.clone();
@@ -170,8 +172,8 @@ public class FirstScene extends BaseScene {
      * 文字
      */
     private void createText(BaseLayer layer) {
-        TextCell textCell = new TextCell("飞机大战");
-        textCell.setTextAlign(Paint.Align.CENTER)
+        TextCell textCell = TextCell.build("飞机大战")
+                .setTextAlign(Paint.Align.CENTER)
                 .setTextSize(30)
                 .setX(getWidth() / 2)
                 .setY(getHeight() / 2)
@@ -194,20 +196,17 @@ public class FirstScene extends BaseScene {
         bg1.setHeight((int) (getWidth() * ratio));
         backGround.addCell(bg1);
         //克隆一张背景图和上面的拼接加长,以便滚动
-        final ImageCell bg2 = (ImageCell) bg1.clone();
+        final ImageCell bg2 = bg1.clone();
         bg2.setY(-bg1.getHeight());
         backGround.addCell(bg2);
         addLayer(backGround);
         //控制2张背景图
-        getCellControl().addCell("bg1", bg1);
-        getCellControl().addCell("bg2", bg2);
-        CellControl.CellProperty bg11 = getCellControl().getCellProperty("bg1").get(0);
-        CellControl.CellProperty bg22 = getCellControl().getCellProperty("bg2").get(0);
+        getCellControl().addCell("bg", bg1);
+        getCellControl().addCell("bg", bg2);
         //向下滚动,速度每秒100dp
-        bg11.setYSpeed(100);
-        bg22.setYSpeed(100);
+        getCellControl().setYSpeed("bg", 100);
         //监听滚动,一张到底后拼接到另一张开头,以做到无限循环
-        CellEventListener cellEventListener = new CellEventListener<ImageCell>() {
+        getCellControl().setCellEventListener("bg", new CellEventListener<ImageCell>() {
             @Override
             public void onCellMove(ImageCell cell, float lastX, float newX, float lastY, float newY, float lastRotation, float newRotation) {
                 BaseCell top = bg1.getY() < bg2.getY() ? bg1 : bg2;
@@ -215,10 +214,7 @@ public class FirstScene extends BaseScene {
                     cell.setY(top.getY() - cell.getHeight() + 1);
                 }
             }
-        };
-        bg11.setCellEventListener(cellEventListener);
-        bg22.setCellEventListener(cellEventListener);
-
+        });
     }
 
 }
